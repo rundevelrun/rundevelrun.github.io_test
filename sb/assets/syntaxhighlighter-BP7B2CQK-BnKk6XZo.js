ce1449bbe1aa61b7a101f73fe768438a828d7da3
ytseo <ytseo>
2024-06-12 14:13:53 +0900

    storybook path change

--- sb/assets/syntaxhighlighter-BP7B2CQK-BnKk6XZo.js
+++
@@ -1,1 +0,0 @@
-import{S as g,c as n,s as u,a as c}from"./index-CduoMdaN.js";import"./iframe-CEIrxd9e.js";import"../sb-preview/runtime.js";import"./index-CEThVCg_.js";import"./index-xnkZ--kd.js";import"./index-DXimoRZY.js";import"./index-9UrncIcR.js";import"./index-DrFu-skq.js";export{g as SyntaxHighlighter,n as createCopyToClipboardFunction,u as default,c as supportedLanguages};
